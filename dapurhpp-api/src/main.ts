c09cf7ed8066a3ee29ea63214009b1ed7e5a693f
ranggautama47 <ranggautama737@gmail.com>
2026-08-14 13:49:34 +0700

    fix(api): adjust NestJS bootstrap configuration

--- dapurhpp-api/src/main.ts
+++ dapurhpp-api/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
     app.enableCors({
-      origin: 'http://localhost:3000',
+      origin: process.env.FRONTEND_URL,
       credentials: true,
     });
 
